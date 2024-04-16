8de9e3ee9815f0ca341dda3a81e2df102b3197b4
Walter <waltels2019@gmail.com>
2024-04-16 15:32:26 -0400

    terminado roles

--- resources/views/files/create.blade.php
+++ resources/views/files/create.blade.php
@@ -7,8 +7,47 @@
             <li class="breadcrumb-item"><a href="#">Forms</a></li>
             <li class="breadcrumb-item active" aria-current="page">Basic Elements</li>
         </ol>
+        @error('description')
+        <script>
+            const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+            }
+            });
+            Toast.fire({
+            icon: "error",
+            title: "No se asigno una descripción del documento"
+            });
+        </script>
+        @enderror
+
+        @error('file')
+        <script>
+            const Toast = Swal.mixin({
+            toast: true,
+            position: "top-end",
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+            }
+            });
+            Toast.fire({
+            icon: "error",
+            title: "No adjunto ningun archivo para enviar"
+            });
+        </script>
+        @enderror
     </nav>
- 
+
     <div class="row justify-content-center">
         <div class="col-md-6 grid-margin stretch-card">
             <div class="card">
@@ -20,13 +59,13 @@
                         <div class="row mb-3">
                             <label class="col-sm-3 col-form-label">Descripcion</label>
                             <div class="col-sm-9">
-                                <input type="text" name="description"  class="form-control"  required placeholder="Descripción">
+                                <input type="text" name="description"  class="form-control"   placeholder="Descripción">
                             </div>
                         </div>
                         <div class="row mb-3">
                             <label class="col-sm-3 col-form-label">Archivo</label>
                             <div class="col-sm-9">
-                                <input type="file" multiple name="file" id="file"  class="form-control" required>
+                                <input type="file" multiple name="file" id="file"  class="form-control" >
                             </div>
                         </div>
                         <button type="submit" class="btn btn-primary me-2">Enviar</button>
